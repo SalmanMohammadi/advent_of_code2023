b3b3897b942fd72a24412bc70e0e0da2b16aa191
Salman Mohammadi <salman.mohammadi@outlook.com>
2023-12-18 00:36:19 +0000

    speeding up day 6 part one

--- 6/solution.py
+++ 6/solution.py
@@ -5,30 +5,23 @@ def get_distance_travelled(time_to_hold:int, race_time:int) -> int:
     assert time_to_hold >= 0 and time_to_hold <= race_time
     return (race_time - time_to_hold) * time_to_hold
 
+def get_record_time(race_time: int, record_distance: int) -> int:
+    # we can solve the get_distance_travelled fn for t (it has quadratic form)
+    return math.floor((race_time + math.sqrt((race_time**2) - (4 * record_distance))) / 2)
+
 def solve_part_one(inpt: str) -> int:
     race_times = list(map(int, inpt[0].split(": ")[1].strip().split()))
-    records = list(map(int, inpt[1].split(": ")[1].strip().split()))
+    record_distances = list(map(int, inpt[1].split(": ")[1].strip().split()))
     winning_combinations = []
-    for race_time, record in zip(race_times, records):
-        n_winning_combinations = 0
-        for i in range(race_time):
-            if get_distance_travelled(i, race_time) > record:
-                n_winning_combinations += 1
-        winning_combinations.append(n_winning_combinations)
+    for race_time, record_distance in zip(race_times, record_distances):
+        max_winning_time = get_record_time(race_time, record_distance)
+        winning_combinations.append(max_winning_time - (race_time - max_winning_time) + 1)
     return functools.reduce(operator.mul, winning_combinations)
 
 def solve_part_two(inpt: str) -> int:
     race_time = int(inpt[0].split(": ")[1].replace(" ", ""))
-    record = int(inpt[1].split(": ")[1].replace(" ", ""))
-
-    max_winning_time = None
-    # we can solve the get_distance_travelled fn for t (it has quadratic form)
-    # using the current record distance, and search from there
-    record_time = (race_time + math.sqrt((race_time**2) - (4 * record))) / 2
-    for i in range(int(record_time), 0, -1):
-        if get_distance_travelled(i, race_time) > record:
-            max_winning_time = i
-            break
+    record_distance = int(inpt[1].split(": ")[1].replace(" ", ""))
+    max_winning_time = get_record_time(race_time, record_distance)
     return max_winning_time - (race_time - max_winning_time) + 1
 
 if __name__ == "__main__":
