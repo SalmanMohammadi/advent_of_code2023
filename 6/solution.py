8980470a881180300f19a0f3591237944fa82cd5
Salman Mohammadi <salman.mohammadi@outlook.com>
2023-12-18 00:27:27 +0000

    day 6 completed

--- 6/solution.py
+++ 6/solution.py
@@ -1,4 +1,5 @@
 import functools, operator, time
+import math
 
 def get_distance_travelled(time_to_hold:int, race_time:int) -> int:
     assert time_to_hold >= 0 and time_to_hold <= race_time
@@ -19,21 +20,26 @@ def solve_part_one(inpt: str) -> int:
 def solve_part_two(inpt: str) -> int:
     race_time = int(inpt[0].split(": ")[1].replace(" ", ""))
     record = int(inpt[1].split(": ")[1].replace(" ", ""))
-    n_winning_combinations = 0
-    for i in range(race_time):
+
+    max_winning_time = None
+    # we can solve the get_distance_travelled fn for t (it has quadratic form)
+    # using the current record distance, and search from there
+    record_time = (race_time + math.sqrt((race_time**2) - (4 * record))) / 2
+    for i in range(int(record_time), 0, -1):
         if get_distance_travelled(i, race_time) > record:
-            n_winning_combinations += 1
-    return n_winning_combinations
+            max_winning_time = i
+            break
+    return max_winning_time - (race_time - max_winning_time) + 1
 
 if __name__ == "__main__":
-    with open("test") as f:
+    with open("input") as f:
         inpt = f.read().split('\n')
         start = time.time()
         res = solve_part_one(inpt)
         end = time.time()
-        print(f"Part one solution: {res} taking {(end - start):.7f}")
+        print(f"Part one solution: {res} taking {(end - start):.7f}s")
 
         start = time.time()
         res = solve_part_two(inpt)
         end = time.time()
-        print(f"Part two solution: {res} taking {(end - start):.7f}")
+        print(f"Part two solution: {res} taking {(end - start):.7f}s")
