8d9db71f3d42e0243e32997ecac9fefc6b68206d
Salman Mohammadi <salman.mohammadi@outlook.com>
2023-12-22 19:36:52 +0000

    optimizing 11

--- 11/solution.py
+++ 11/solution.py
@@ -1,5 +1,7 @@
 import itertools
 from typing import List
+import time
+
 def get_row_col_sizes(
         expansion_factor: int, 
         observatory_data: List[List[str]]
@@ -13,6 +15,7 @@ def get_row_col_sizes(
 
 if __name__ == "__main__":
     with open("input") as f:
+        start = time.time()
         observatory_data = f.read().split('\n')
         galaxy_idxs = []
         for i in range(len(observatory_data[0])):
@@ -27,15 +30,19 @@ if __name__ == "__main__":
         paths_total_milx = 0
 
         for (galaxy_1_idx, galaxy_2_idx) in galaxy_combinations:
-            distance_row = sum([row_sizes_2x[galaxy_1_idx[0] + x] for x in range(0, galaxy_2_idx[0] - galaxy_1_idx[0], 1 if galaxy_2_idx[0] > galaxy_1_idx[0] else -1)])
-            distance_col = sum([column_sizes_2x[galaxy_1_idx[1] + x] for x in range(0, galaxy_2_idx[1] - galaxy_1_idx[1], 1 if galaxy_2_idx[1] > galaxy_1_idx[1] else -1)])
+            row_idxs = range(0, galaxy_2_idx[0] - galaxy_1_idx[0], 1 if galaxy_2_idx[0] > galaxy_1_idx[0] else -1)
+            col_idxs = range(0, galaxy_2_idx[1] - galaxy_1_idx[1], 1 if galaxy_2_idx[1] > galaxy_1_idx[1] else -1)
+            
+            distance_row = sum([row_sizes_2x[galaxy_1_idx[0] + x] for x in row_idxs])
+            distance_col = sum([column_sizes_2x[galaxy_1_idx[1] + x] for x in col_idxs])
             paths_total_2x += (distance_col + distance_row)
 
-
-            distance_row = sum([row_sizes_milx[galaxy_1_idx[0] + x] for x in range(0, galaxy_2_idx[0] - galaxy_1_idx[0], 1 if galaxy_2_idx[0] > galaxy_1_idx[0] else -1)])
-            distance_col = sum([column_sizes_milx[galaxy_1_idx[1] + x] for x in range(0, galaxy_2_idx[1] - galaxy_1_idx[1], 1 if galaxy_2_idx[1] > galaxy_1_idx[1] else -1)])
+            distance_row = sum([row_sizes_milx[galaxy_1_idx[0] + x] for x in row_idxs])
+            distance_col = sum([column_sizes_milx[galaxy_1_idx[1] + x] for x in col_idxs])
             paths_total_milx += (distance_col + distance_row)
-
+            
+        end = time.time()
+        print(f"Took {(end - start):5f}s")
         print(f"Part one: {paths_total_2x}")
         print(f"Part two: {paths_total_milx}")
 
